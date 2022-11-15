bd2731cb32dfd83505a8d72c4fe6dc2652cb5cb3
achrafbelhajali89 <118275858+achrafbelhajali89@users.noreply.github.com>
2022-11-15 15:50:23 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,5 +6,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
   key_name      = "devopskey"
-  
+  tags = {
+    Name = "ec2-achraf"
+  }
 }
